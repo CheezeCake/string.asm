07d2f5aef9f5482887bc400e41a44e0bca4f4de7
CheezeCake <emmanuel.nicolet@gmail.com>
2013-06-02 21:07:12 +0200

    suppr instructions inutiles

--- strlen_32.s
+++ strlen_32.s
@@ -6,9 +6,7 @@
 _strlen_asm:
 	movl	4(%esp),%esi
 count_loop:
-	xorl	%eax,%eax
-	movb	(%esi),%al
-	cmpb	$0,%al
+	cmpb	$0,%(%esi)
 	je      end
 	incl	%esi
 	jmp     count_loop
--- strlen_64.s
+++ strlen_64.s
@@ -6,9 +6,7 @@
 _strlen_asm:
 	movq	%rdi,%rsi #s = str
 count_loop:
-	xorq	%rax,%rax
-	movb	(%rsi),%al
-	cmpb	$0,%al
+	cmpb	$0,(%rsi)
 	je      end
 	incq	%rsi
 	jmp     count_loop
