25495588d72d85441a5f6fd57af4f61675bb1398
CheezeCake <emmanuel.nicolet@gmail.com>
2013-06-11 08:04:55 +0200

    strcat

---
+++ strcat_64.s
@@ -0,0 +1,19 @@
+.text
+.globl _strcat_asm
+
+#char* strcat(char* dest, const char* src);
+
+_strcat_asm:
+	movq	%rdi,%rax
+get_to_end:
+	cmpb	$0,(%rdi)
+	je      copy_loop
+	incq	%rdi
+	jmp     get_to_end
+copy_loop:
+	movsb
+	cmpb	$0,(%rsi)
+	je      end
+	jmp     copy_loop
+end:
+	ret
