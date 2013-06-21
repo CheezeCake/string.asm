195a14973698a8fecf48717aef25ba41bff70254
CheezeCake <emmanuel.nicolet@gmail.com>
2013-06-21 19:48:55 +0200

    fix

--- include/string_asm.h
+++ include/string_asm.h
@@ -1,11 +1,11 @@
 #ifndef _STRING_ASM_H_
 #define _STRING_ASM_H_
 
-#include <sys/types>
+#include <sys/types.h>
 
 char* strcat_asm(char*, const char*);
 int strcmp_asm(const char*, const char*);
-char* strcpy(char*, const char*);
-size_t strlen(const char*);
+char* strcpy_asm(char*, const char*);
+size_t strlen_asm(const char*);
 
 #endif
